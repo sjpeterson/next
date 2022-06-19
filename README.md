1cbf935958a1b925e67f2a74b34117dffd7a9ad3
Stefan Peterson <stefan.j.peterson@gmail.com>
2022-06-19 16:00:33 +0200

    non-working WIP

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # next
+
+Yet another to-do application.
+
+## Installation
+
+    stack install
+
+This installs the executable `nn` for the current user.
+
+### Usage
+
+Next organizes tasks to be done in separate contexts. Typical contexts might
+be "work" and "private". The default context is aptly named "default". To get
+the name of the active context, run
+
+    nn context
+
+To rename the current context to "work", run
+
+    nn context -r work
+
+To create a new context called "private" and switch to it, run
+
+    nn context -n private
+
+To switch back to the work context
+
+    nn context work
+
+
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,53 @@
 module Main where
 
-import Lib
+-- import Paths_nn ( version )
+import Data.Version ( showVersion )
+import Options.Applicative ( Parser
+                           , (<**>)
+                           , (<|>)
+                           , command
+                           , execParser
+                           , header
+                           , helper
+                           , info
+                           , flag'
+                           , fullDesc
+                           , long
+                           , progDesc
+                           , short
+                           )
+
+data Options = PrintVersionOptions ()
+             | CommandOptions Command
+             | InteractiveOptions ()
+
+data Command = Context ()
+
+options :: Parser Options
+options = printVersionOptions <|> commandOptions <|> interactiveOptions
+
+printVersionOptions :: Parser Options
+printVersionOptions = PrintVersionOptions
+    <$> flag' () (long "version" <> short 'v')
+
+commandOptions :: Parser Options
+commandOptions = subparser
+    ( command "context" (info contextCommand (progDesc "Handle next context"))
+    )
+
+contextCommand :: IO ()
+contextCommand 
+
+interactiveOptions :: Parser Options
+interactiveOptions = InteractiveOptions
 
 main :: IO ()
-main = someFunc
+main = runApp' =<< execParser opts
+  where
+    opts = info (options <**> helper)
+        ( fullDesc
+       <> progDesc ""
+       <> header "next"
+        )
+
+    runApp' = putStrLn . show
